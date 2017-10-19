159e1df54beae6cae489fb7a344085b0ca0d6d17
Danyelle B <barrettd1@hawkmail.newpaltz.edu>
2017-10-19 14:50:01 -0400

    server!

--- jquery-mockup/src/game.ts
+++ jquery-mockup/src/game.ts
@@ -5,23 +5,37 @@ export class Quote{
 }
 
 export class Player{
-    name: string;
+    name: string = "Moshe Plotkin";
     quotes: Quote[] = [];
     score: number = 0;
 
     drawQuotes(){
-        $("#my-quotes").html(this.quotes.map(x=> '<li class="list-group-item">${x.text}</li>').join(""))
+        $("#my-quotes").html(
+            this.quotes.map(x => `<li class="list-group-item">${x.text}</li>`).join("")
+        )
     }
 }
 
 export class Room{
-    players: Player[] = [];
+    players: Player[] = [new Player(), new Player()];
     dealer: Player;
     picture: string = "https://media4.s-nbcnews.com/j/newscms/2017_07/1903576/170215-chicken-farm-mn-1630_c65475166849611a3c0207983317eab4.nbcnews-ux-320-320.jpg";
     quotes: Quote[] = [];
 
     drawPicture(){
         $("#picture").attr("src", this.picture);
+    }
+
+    drawQuotes(){
+        $("#played-quotes").html(
+            this.quotes.map(x => `<li class="list-group-item">${x.text}</li>`).join("")
+        )
+    }
+
+    drawPlayers(){
+        $("#players").html(
+            this.players.map(x => `<li class="list-group-item">${x.name}</li>`).join("")
+        )
     }
 }
 
@@ -47,6 +61,8 @@ const me = new Player();
 let i = 0;
 room.picture = game.pictures[i];
 room.drawPicture();
+room.drawQuotes();
+room.drawPlayers();
 
 me.quotes = game.quotes;
 me.drawQuotes();
