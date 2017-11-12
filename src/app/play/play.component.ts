97cb5290e516953683bb41dcfd56e9dca7c4b2f6
Danyelle B <barrettd1@hawkmail.newpaltz.edu>
2017-11-12 15:42:51 -0500

    completely in angular

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -4,29 +4,46 @@ import { Room, Player, Quote } from '../models/game';
 import * as $ from 'jquery';
 
 @Component({
-  selector: 'app-play',
-  templateUrl: './play.component.html',
-  styleUrls: ['./play.component.scss']
+    selector: 'app-play',
+    templateUrl: './play.component.html',
+    styleUrls: ['./play.component.scss']
 })
+
 export class PlayComponent implements OnInit {
 
-  apiRoot = "//localhost:3000"
+    apiRoot = "//localhost:3000"
 
-  room = new Room();
+    room = new Room();
+    me = new Player();
 
-  constructor() { }
+     constructor() { }
 
-  ngOnInit() {
-    setInterval(() => this.update(), 1000)
-  }
+    ngOnInit() {
+        setInterval(() => this.update(), 1000)
+        $.getJSON(this.apiRoot+ "/game/quotes").done( data => {
+          this.me.quotes = data;
+        })
+    }
 
-  update(){
-    $.get(this.apiRoot + "/game/room/picture").done( data => {
-        this.room.picture = data;
-    });
-    $.getJSON(this.apiRoot +  "/game/room/quotes").done( data => {
-        this.room.quotes = data;
-    });
-  }
+    update(){
+       $.get(this.apiRoot + "/game/room/picture").done( data => {
+          this.room.picture = data;
+      });
+      $.getJSON(this.apiRoot +  "/game/room/quotes").done( data => {
+          this.room.quotes = data;
+      });
+    }
+
+    flipPicture(e: MouseEvent){
+        e.preventDefault();
+        $.post(this.apiRoot + "/game/room/picture")
+    }
+
+    submitQuote(e: MouseEvent, quote: Quote, i: number){
+        e.preventDefault();
+        const data = { text: quote.text };
+        $.post(this.apiRoot + "/game/room/quotes", data);
+        this.me.quotes.splice(i, 1);
+    }
 
 }
